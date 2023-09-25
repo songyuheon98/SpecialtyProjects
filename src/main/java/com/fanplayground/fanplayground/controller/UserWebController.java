c393bcfefc6b7ec2ea5489a972dbb44ca2e1c348
songyuheon98 <songyuheon2750@gmail.com>
2023-09-25 15:26:43 +0900

    로그인 & 회원가입 & 회원탈퇴 & 로그아웃 기능 구현 완료

--- src/main/java/com/fanplayground/fanplayground/controller/UserWebController.java
+++ src/main/java/com/fanplayground/fanplayground/controller/UserWebController.java
@@ -1,7 +1,11 @@
 package com.fanplayground.fanplayground.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class UserWebController {
@@ -26,6 +30,20 @@ public class UserWebController {
         return "/user/escape";
     }
 
+    @GetMapping("/user/logout")
+    public String logout() {
+        /**
+         * 쿠키 삭제 기능 추가
+         */
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletResponse response = attributes.getResponse();
+        Cookie cookie = new Cookie("Authorization", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "/user/login";
+    }
 
     @GetMapping("/main")
     public String mainPage() {
